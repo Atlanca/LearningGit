9b3f5f39daadece98d8ace014737e4a6e985bf30
Alex Tao <talex@student.chalmers.se>
2017-03-25 00:06:48 +0100

    Quick fixes

---
+++ app/src/main/java/chalmers/talex/learninggit/DietActivities/MainActivity.java
@@ -0,0 +1,15 @@
+package chalmers.talex.learninggit.DietActivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import chalmers.talex.learninggit.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
